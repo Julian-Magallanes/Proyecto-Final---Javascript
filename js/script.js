a7a7daad6739115e56e6fe8c8a4b8068cc14791e
Julian Magallanes <julianmagallanes1994@gmail.com>
2022-05-18 18:49:32 -0300

    agregando operadores

--- js/script.js
+++ js/script.js
@@ -49,19 +49,17 @@ class MaterialesCimiento {
         this.piedraBola = piedraBola;
     }
 }
-//defino arrays vacios
 
+//defino arrays vacios
 let materialesB= []
 let materialesC= []
 
 //condicionales para consultar el localStore
-
 if(localStorage.getItem('MaterialesB')){
     materiales = JSON.parse(localStorage.getItem('MaterialesB'))
 }else {
     localStorage.setItem('MaterialesB', JSON.stringify(materialesB))
 }
-
 
 if(localStorage.getItem('MaterialesC')){
     materialesC = JSON.parse(localStorage.getItem('MaterialesC'))
@@ -70,11 +68,9 @@ if(localStorage.getItem('MaterialesC')){
 }
 
 //defino la variable en el id del formulario html
-
 let formulario = document.getElementById("idForm")
 
 //Creo el evento de escucha para el formulario
-
 formulario.addEventListener('submit',(event) => {
     event.preventDefault()
 
@@ -83,7 +79,6 @@ formulario.addEventListener('submit',(event) => {
 })
 
 //Creo las funciones dentro del formulario => donde se elige el sistema y a partir de ese dato con las dimensiones calcula la cant de materiales necesarios.
-
 const sistemaElegido = () => {
     if(document.getElementById('btn-bases').checked){
         console.log("usted eligio bases asiladas")
@@ -96,10 +91,13 @@ const sistemaElegido = () => {
 }
 
 const bases = () =>{
-
-    let alto = document.getElementById('alto').value;
+    //uso operador AND.
+    let alto = document.getElementById('alto').value ;
+    alto.length === 0 && alert("Usted no incluyo el alto")
     let ancho = document.getElementById('ancho').value;
+    ancho.length === 0 && alert("Usted no incluyo el ancho")
     let largo = document.getElementById('largo').value;
+    largo.length === 0 && alert("Usted no incluyo el largo")
 
     let resultado1 = cemento(alto, largo, ancho, rendCemento)
     let resultado2 = arena(alto, largo, ancho, rendArena)
@@ -107,31 +105,33 @@ const bases = () =>{
     
     resume__text.innerHTML = `<h2>Bases Aisladas:</h2> <br> <p>Usted necesitara: <br>${ resultado1 } bolsas de cemento <br>${ resultado2 } m3 de arena <br>${ resultado3 } m3 de ripio</p>`
 
-    //Creo un array y lo guardo en el localStorage
+//Creo un array y lo guardo en el localStorage
     const valor = new MaterialesBases(resultado1,resultado2,resultado3)
     materialesB.push(valor)
     console.log(materialesB)
     localStorage.setItem('MaterialesB', JSON.stringify(materialesB))
-
 }
 
 const ciclopeo =() =>{
+    //uso operador AND.
     let alto = document.getElementById('alto').value;
+    alto.length === 0 && alert("Usted no incluyo el alto")
     let ancho = document.getElementById('ancho').value;
+    ancho.length === 0 && alert("Usted no incluyo el ancho")
     let largo = document.getElementById('largo').value;
+    largo.length === 0 && alert("Usted no incluyo el largo")
 
     let resultado3 = hormigon(alto,largo,ancho, rendHormigon)
     let resultado4 = piedrabola(alto,largo,ancho, rendPiedraBola)
 
     resume__text.innerHTML = `<h2>Cimiento Ciclopeo:</h2> <br> <p>Usted necesitara: <br>${ resultado3 } m3 de hormigon elaborado <br>${ resultado4 } m3 de piedra bola </p>`
 
-    //Creo un array y lo guardo en el localStorage
+//Creo un array y lo guardo en el localStorage
     const valor = new MaterialesCimiento(resultado3,resultado4)
     materialesC.push(valor)
     console.log(materialesC)
     localStorage.setItem('MaterialesC', JSON.stringify(materialesC))
 }
-
 
 const hormigon = (alto, largo, ancho, rendHormigon) => alto * largo * ancho * rendHormigon;
 
@@ -143,10 +143,7 @@ const arena = (alto, largo, ancho, rendArena) => alto * largo * ancho * rendAren
 
 const ripio = (alto, largo, ancho, rendRipio) => alto * largo * ancho * rendRipio;
 
-
-
 //Creo el evento a partir del boton de historial => donde lee el localStorage y lo inserta al html
-
 document.getElementById("btn-calcular2").addEventListener("click", resumenTotal)
 
 let botonResumen = document.getElementById("btn-calcular2")
@@ -154,7 +151,7 @@ let botonResumen = document.getElementById("btn-calcular2")
 function resumenTotal () {
     let historial=JSON.parse( localStorage.getItem("MaterialesB"))
 
-    historial.forEach(materiales => {
+    historial.forEach (materiales => {
         resume__text2.innerHTML += `
         <p>BASE AISLADA
         <br>${materiales.cemento} bolas de cemento
@@ -162,19 +159,33 @@ function resumenTotal () {
         <br>${materiales.ripio} m3 de ripio</p>
         `
     });
-    let historial2=JSON.parse( localStorage.getItem("MaterialesC"))
-
+    let historial2=JSON.parse( localStorage.getItem("MaterialesC")) 
     historial2.forEach(materialesC => {
         resume__text3.innerHTML += `
         <p>CIMIENTO CICLOPEO
         <br>${materialesC.hormigon} m3 de hormigon
         <br>${materialesC.piedraBola} m3 de piedraBola</p>
         `
-    });    
+    });
+    /*
+    let historial1=JSON.parse( localStorage.getItem("MaterialesB"))
+    let historial2=JSON.parse( localStorage.getItem("MaterialesC"))
+
+    const historial = [...historial1,...historial2]
+    historial.forEach(materiales => {
+        resume__text3.innerHTML += `
+        <p>Usted necesita
+        <br>${materiales.hormigon} m3 de hormigon
+        <br>${materiales.piedraBola} m3 de piedraBola</p>
+        <br>${materiales.cemento} bolsas de cemento
+        <br>${materiales.arena} m3 de arena
+        <br>${materiales.ripio} m3 de ripio</p>
+    `
+    });
+    */
 }
 
 //Creo el evento a partir del boton de borrar historial => donde remueve los datos del localStorage y elimina el html creado anteriormente
-
 document.getElementById("btn-calcular3").addEventListener("click", borrarHistorial)
 let botonBorrar = document.getElementById("btn-calcular3")
 
